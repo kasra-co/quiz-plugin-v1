eb54cac1d933195040ea42d8a86358817b7aedc7
Dan Ross <ross9885@gmail.com>
2015-03-30 20:48:42 +0300

    Correction: get_option gets all options, not a single option.

--- src/plugin/admin-settings.php
+++ src/plugin/admin-settings.php
@@ -17,6 +17,7 @@ function renderArticleServiceSection() { ?>
 	<p>Specify the URL of the article service. This will be used for saving and retrieving quizzes. Example: <code>http://localhost:3001</code>. This configuration should be obsoleted once we have a service discovery process.</p>
 <?php }
 
-function renderEndpointURLControl() { ?>
-	<input name="menapost-quiz-options[menapost_quiz_endpoint]" id="quiz-endpoint" type="text">
-<?php }
+function renderEndpointURLControl() {
+	$options = get_option( 'menapost-quiz-options' );
+	?><input name="menapost-quiz-options[menapost_quiz_endpoint]" id="quiz-endpoint" type="text" value="<?= $options[ 'menapost_quiz_endpoint' ] ?>"/><?php
+}
