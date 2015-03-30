2ae6285bf588327a53ea8077bc334ef51394bc8a
Dan Ross <ross9885@gmail.com>
2015-03-30 19:09:27 +0300

    Load quiz data from the article service. Throw an error if the article service endpoint has not been configured.

--- src/plugin/lib/load-quiz.php
+++ src/plugin/lib/load-quiz.php
@@ -2,5 +2,40 @@
 
 // Returns a post's quiz as an associative array
 function loadQuiz( $postId ) {
+
+	function fetchArticleQuiz( $endpoint, $postId ) {
+		$articleSlug = getQuizArticleSlugForPost( $postId );
+		$article = httpGetObject( rtrim( $endpoint, '/' ) . '/article/' . $articleSlug );
+		return $article->quiz;
+	}
+
+	function getQuizArticleSlugForPost( $postId ) {
+		return get_post_meta( $postId, 'quiz-article-slug', true );
+	}
+
+	function httpGetObject( $url ) {
+		$curler = curl_init( $url );
+		curl_setopt( $curler, CURLOPT_HEADER, 0 );
+		curl_setopt( $curler, CURLOPT_HTTPHEADER, [
+			'Accept: application/json',
+			'Accept-Charset: utf-8'
+		]);
+		curl_setopt( $curler, CURLOPT_RETURNTRANSFER, 1 );
+		$response = curl_exec( $curler );
+		curl_close( $curler );
+
+		return json_decode( $response );
+	}
+
+	// get service info
+	$endpoint = get_option( 'quiz_endpoint', null );
+
+	if( $endpoint === null ) {
+		trigger_error( 'Menapost quiz: article endpoint undefined', E_USER_WARNING );
+		return;
+	}
+
+	$quiz = fetchArticleQuiz( $endpoint, $postId );
+
 	return json_decode( file_get_contents( __DIR__ . '/../config/demo-data.json' ), true );
 }
