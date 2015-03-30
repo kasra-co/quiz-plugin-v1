b713e20cf26fa13792e0d812dba03ade4f8e7bd3
Dan Ross <ross9885@gmail.com>
2015-03-30 11:43:49 +0300

    Fix JSON demo data for PHP's parser. Read quiz data as assoc array so that wp_localize can process it as is

--- src/plugin/lib/load-quiz.php
+++ src/plugin/lib/load-quiz.php
@@ -1,5 +1,6 @@
 <?php
 
+// Returns a post's quiz as an associative array
 function loadQuiz( $postId ) {
-	return json_decode( file_get_contents( __DIR__ . '../config/demo-data.json' ));
+	return json_decode( file_get_contents( __DIR__ . '/../config/demo-data.json' ), true );
 }
