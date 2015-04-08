be872e36fb5c62d042b0c3682696d1581bf99641
Dan Ross <ross9885@gmail.com>
2015-04-08 11:04:46 +0300

    Quiz FEM exports a quiz object, not an object containing a quiz

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
 let React = require( "react" );
-let Quiz = require( "quiz" ).Quiz;
+let Quiz = require( "quiz" );
 
 // Expects quizData to have been initialized by something like wp_localize_script
-React.render( <Quiz quiz={ window.quizContent } title={ window.quizTitle }/>, document.getElementById( "quiz-mount-point" ));
+React.render(
+<Quiz
+	quiz={ window.quizContent }
+	quizTitle={ window.quizTitle }
+/>,
+document.getElementById( "quiz-mount-point" ));
