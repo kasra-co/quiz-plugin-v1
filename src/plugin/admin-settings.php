f0afd35b5b3039a98da4ed3b06df9fdfb46f029c
Dan Ross <ross9885@gmail.com>
2015-04-08 11:20:58 +0300

    Add admin panel for configuring endpoint url, only load editor assets on editor page

--- src/plugin/admin-settings.php
+++ src/plugin/admin-settings.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once( __DIR__ . '/lib/get-endpoint.php' );
+
 // See http://ottopress.com/2009/wordpress-settings-api-tutorial/
 
 add_action( 'admin_init', function() {
@@ -15,18 +17,11 @@ add_action( 'admin_init', function() {
 	add_settings_field( 'quizEndpoint', __( 'Quiz service endpoint URL' ), 'renderEndpointURLControl', 'menapost-quiz', 'menapost-quiz-article-service' );
 });
 
-function renderArticleServiceSection() { ?>
-	<p>Specify the URL of the quiz service. This will be used for saving and retrieving quizzes. Example: <code>http://localhost:3001</code>. This configuration should be obsoleted once we have a service discovery process.</p>
-<?php }
+function renderArticleServiceSection() {
+	$instructions = __( 'Specify the URL of the quiz service. This will be used for saving and retrieving quizzes. Example: <code>http://localhost:3001</code>. This configuration should be obsoleted once we have a service discovery process.' );
+	?><p><?= $instructions ?></p><?php
+}
 
 function renderEndpointURLControl() {
-	$options = get_option( 'menapost-quiz-options' );
-
-	if( isset( $options[ 'quizEndpoint' ])) {
-		$value = $options[ 'quizEndpoint' ];
-	} else {
-		$value = '';
-	}
-
-	?><input name="menapost-quiz-options[quizEndpoint]" id="quiz-endpoint" type="text" value="<?= $value ?>"/><?php
+	?><input name="menapost-quiz-options[quizEndpoint]" id="quiz-endpoint" type="text" value="<?= getEndpoint() ?>"/><?php
 }
--- src/plugin/backend.php
+++ src/plugin/backend.php
@@ -1,4 +1,12 @@
 <?php
 
-require_once( __DIR__ . '/admin-settings.php' );
-require_once( __DIR__ . '/admin-settings-page.php' );
+require_once( __DIR__ . '/quiz-meta-box.php' );
+require_once( __DIR__ . '/quiz-updator.php' );
+
+add_action( 'admin_enqueue_scripts', function( $hook ) {
+	global $post;
+
+	// Only load the quiz editor on the post edit page
+	wp_enqueue_script( 'quiz-editor', plugin_dir_url( __DIR__ ) . plugin_basename( __DIR__ . '/static/quiz-editor.min.js', [], VERSION, true ));
+	wp_enqueue_style( 'quiz-editor', plugin_dir_url( __DIR__ ) . plugin_basename( __DIR__ . '/static/quiz-editor.css' ), [], VERSION );
+});
