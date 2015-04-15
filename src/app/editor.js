8087ffa12caace8a4ad0532ee02593f4a1f2d33e
Dan Ross <ross9885@gmail.com>
2015-04-15 11:59:29 +0300

    Cleanup. Break closures out into global functions to work around PHP redefinition spaz attack.

--- src/app/editor.js
+++ src/app/editor.js
@@ -2,7 +2,15 @@ var React = require( "react" );
 var QuizEditor = require( "quiz-editor" ).Quiz;
 var quiz;
 
-React.render( <QuizEditor updateQuiz={ function( newQuiz ) { quiz = newQuiz; }}/>, document.getElementById( "quiz-editor" ));
+var quizApp = (
+	<QuizEditor
+		updateQuiz={ function( newQuiz ) {
+			quiz = newQuiz;
+		}}
+	/>
+);
+
+React.render( quizApp, document.getElementById( "quiz-editor" ));
 
 document.getElementById( "post" ).addEventListener( "submit", function( event ) {
 	$( event.target ).append( "<input type=\"hidden\" name=\"quiz\" value=\"" + JSON.stringify( quiz ) + "\"/>" );
--- src/plugin/lib/load-quiz.php
+++ src/plugin/lib/load-quiz.php
@@ -2,51 +2,51 @@
 
 require_once( __DIR__ . '/get-endpoint.php' );
 
-// Returns a post's quiz as an associative array
-function loadQuiz( $postSlug ) {
+// Get a quiz for a post by it's slug, or undefined if one is not found
+function fetchArticleQuiz( $endpoint, $postSlug ) {
+	$article = httpGetObject( rtrim( $endpoint, '/' ) . '/article/' . $postSlug );
 
-	function fetchArticleQuiz( $endpoint, $postSlug ) {
-		$article = httpGetObject( rtrim( $endpoint, '/' ) . '/article/' . $postSlug );
-
-		if( !$article ) {
-			return null;
-		}
-
+	if( isset( $article ) && isset( $article->quiz )) {
 		return $article->quiz;
 	}
+}
 
-	function httpGetObject( $url ) {
-		$context = stream_context_create([
-			'http' => [
-				'method' => 'GET',
-				'header' => [
-					'Accept: application/json',
-					'Accept-Charset: utf-8'
-				]
+function httpGetObject( $url ) {
+	$context = stream_context_create([
+		'http' => [
+			'method' => 'GET',
+			'header' => [
+				'Accept: application/json',
+				'Accept-Charset: utf-8'
 			]
-		]);
+		]
+	]);
 
-		$response = @file_get_contents( $url, false, $context );
-		if( $response === false ) {
-			return null;
-		}
+	$response = @file_get_contents( $url, false, $context );
 
-		$matches = [];
-		preg_match( '/^\S+ (\d+)/', $http_response_header[ 0 ], $matches );
-		$status = (int) $matches[ 0 ];
-		if( $status !== 200 ) {
-			return null;
-		}
+	$matches = [];
+	// $http_response_header is a magic PHP var, dumped into our scope when file_get_contents is called with a url
+	preg_match( '/^\S+ (\d+)/', $http_response_header[ 0 ], $matches );
+	$status = (int) $matches[ 0 ];
 
-		return json_decode( $response );
+	if( $status >= 500 ) {
+		trigger_error( "Server error in article service:\n" . $response );
+	} elseif( $response === false ) {
+		return;
 	}
+
+	return json_decode( $response );
+}
+
+// Returns a post's quiz as an associative array
+function loadQuiz( $postSlug ) {
 
 	$endpoint = getEndpoint();
 
 	$article = fetchArticleQuiz( $endpoint, $postSlug );
 
 	if( !$article ) {
-		return null;
+		return;
 	}
 
 	return $article->quiz;
