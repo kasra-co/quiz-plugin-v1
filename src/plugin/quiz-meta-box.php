f2b850052288a834da6045e368be4bcc74722610
Dan Ross <ross9885@gmail.com>
2015-04-16 04:54:14 +0300

    Use post slug, not id for referencing articles

--- src/plugin/quiz-meta-box.php
+++ src/plugin/quiz-meta-box.php
@@ -7,7 +7,7 @@ add_action( 'add_meta_boxes', function() {
 
 	$render = function () {
 		global $post;
-		renderQuizEditor( loadQuiz( $post->ID ));
+		renderQuizEditor( loadQuiz( $post->post_name ));
 	};
 
 	add_meta_box( 'quizMeta', __( 'Quiz' ), $render, 'post' );
