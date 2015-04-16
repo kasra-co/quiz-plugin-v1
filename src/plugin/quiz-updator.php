a2821839c90e5fad95dc5893ab44fcdb7744a411
Dan Ross <ross9885@gmail.com>
2015-04-16 04:55:43 +0300

    Check to make sure we are saving a quiz before trying to talk to the article service

--- src/plugin/quiz-updator.php
+++ src/plugin/quiz-updator.php
@@ -3,6 +3,10 @@
 require_once( __DIR__ . '/lib/save-quiz.php' );
 
 add_action( 'save_post_post', function( $post_id, $post, $update ) {
+	if( !isset( $_POST ) || !isset( $_POST[ 'quiz' ] )) {
+		return;
+	}
+
 	if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 		return;
 	}
@@ -11,18 +15,19 @@ add_action( 'save_post_post', function( $post_id, $post, $update ) {
 		return;
 	}
 
-	if( !isset( $_POST ) ) {
-		return;
-	}
-
 	$quiz = json_decode( htmlspecialchars_decode( wp_unslash( $_POST[ 'quiz' ]), ENT_COMPAT ));
 
-	if( !$quiz ) {
+	if( !isset( $quiz )) {
 		trigger_error( 'Quiz: Bad JSON' );
 		return;
 	}
 
-	saveQuiz( $post->ID, $quiz );
+	// This is not a quiz article, it has no quiz
+	if( $quiz === null ) {
+		return;
+	}
+
+	saveQuiz( $post->post_name, $quiz );
 }, 10, 3); // Tell WP that we are using the 3 arg form
 
 add_action( 'wp_ajax_quiz-add-image', function() {
