dc0dcddcf2e88d0cf88c1322d9a09e4a997d9920
Dan Ross <ross9885@gmail.com>
2015-04-20 15:30:59 +0300

    GET quiz articles to load quizes. Do not strip slashes, fixes #8

--- src/plugin/lib/load-quiz.php
+++ src/plugin/lib/load-quiz.php
@@ -1,7 +1,42 @@
 <?php
 
+require_once ( __DIR__ . '/get-endpoint.php' );
+
+function httpGetObject( $url ) {
+	$request = curl_init();
+
+	curl_setopt( $request, CURLOPT_URL, $url );
+	curl_setopt( $request, CURLOPT_HTTPHEADER, Array(
+		'Accept: application/json'
+	));
+	curl_setopt( $request, CURLOPT_FOLLOWLOCATION, true );
+	curl_setopt( $request, CURLOPT_RETURNTRANSFER, true );
+
+	$response = curl_exec( $request );
+	$status = curl_getinfo( $request, CURLINFO_HTTP_CODE );
+	curl_close( $request );
+
+	if( $status !== 200 ) {
+		trigger_error( "Menapost quiz: Failed to load quiz, article service returned $status: $response", E_USER_WARNING );
+		return;
+	}
+
+	$article = json_decode( $response );
+	return $article;
+};
+
+function fetchArticleQuiz( $endpoint, $postSlug ) {
+	$article = httpGetObject( rtrim( $endpoint, '/' ) . '/article/' . $postSlug . '/quiz');
+
+	if( isset( $article ) && isset( $article->quiz )) {
+		return $article->quiz;
+	}
+};
+
 // Returns a post's quiz as an associative array
 function loadQuiz( $post ) {
-	$quiz =  json_decode( get_post_meta( $post->ID, 'quiz', true ));
-	return $quiz;
+
+	$endpoint = getEndpoint();
+
+	return fetchArticleQuiz( $endpoint, $post->post_name );
 }
--- src/plugin/lib/save-quiz.php
+++ src/plugin/lib/save-quiz.php
@@ -1,37 +1,41 @@
 <?php
 
+require_once ( __DIR__ . '/get-endpoint.php' );
+
+function httpPostObject( $url, $data ) {
+	$request = curl_init();
+
+	curl_setopt( $request, CURLOPT_URL, $url );
+	curl_setopt( $request, CURLOPT_POST, true );
+	curl_setopt( $request, CURLOPT_POSTFIELDS, json_encode( $data ));
+	curl_setopt( $request, CURLOPT_HTTPHEADER, Array(
+		'Content-Type: application/json',
+		'Accept: application/json'
+	));
+	curl_setopt( $request, CURLOPT_FOLLOWLOCATION, true );
+	curl_setopt( $request, CURLOPT_RETURNTRANSFER, true );
+
+	$response = curl_exec( $request );
+	$status = curl_getinfo( $request, CURLINFO_HTTP_CODE );
+	curl_close( $request );
+
+	if( $status !== 200 ) {
+		trigger_error( "Menapost quiz: Failed to save quiz, article service returned $status: $response", E_USER_WARNING );
+		return;
+	}
+
+	$article = json_decode( $response );
+	return $article->quiz;
+};
+
 require_once( __DIR__ . '/get-endpoint.php' );
 
 function saveQuiz( $post, $quiz ) {
-	$httpPostObject = function( $url, $data ) {
-		$request = curl_init();
-
-		curl_setopt( $request, CURLOPT_URL, $url );
-		curl_setopt( $request, CURLOPT_POST, true );
-		curl_setopt( $request, CURLOPT_POSTFIELDS, json_encode( $data ));
-		curl_setopt( $request, CURLOPT_HTTPHEADER, Array(
-			'Content-Type: application/json',
-			'Accept: application/json'
-		));
-		curl_setopt( $request, CURLOPT_FOLLOWLOCATION, true );
-		curl_setopt( $request, CURLOPT_RETURNTRANSFER, true );
-
-		$response = curl_exec( $request );
-		$status = curl_getinfo( $request, CURLINFO_HTTP_CODE );
-		curl_close( $request );
-
-		if( $status !== 200 ) {
-			trigger_error( "Menapost quiz: Failed to save quiz, article service returned $status: $response", E_USER_WARNING );
-			return;
-		}
-
-		return $response;
-	};
 
 	$article = [
 		'slug' => $post->post_name,
 		'quiz' => $quiz
 	];
 
-	return $httpPostObject( getEndpoint() . "/article/quiz-article", $article );
+	return httpPostObject( getEndpoint() . "/article/quiz-article", $article );
 }
