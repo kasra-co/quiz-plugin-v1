ea3448741dd5ae6b2789c4500814091c5c1d0cfc
Dan Ross <ross9885@gmail.com>
2015-04-21 11:27:44 +0300

    Pass article short url to quiz

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,7 @@ React.render(
 	<Quiz
 		quiz={ window.quizContent }
 		quizTitle={ window.quizTitle }
+		shortUrl={ window.shortUrl }
 		siteUrl={ window.siteUrl }
 	/>,
 	document.getElementById( "quiz-mount-point" )
