5edf39bfdbc8d7dcbdf8abdf786b9dc1c5595e12
Dan Ross <ross9885@gmail.com>
2015-05-18 10:38:34 +0300

    Remove options page: not needed

--- src/plugin/quiz-adaptor.php
+++ src/plugin/quiz-adaptor.php
@@ -10,15 +10,6 @@ define( 'QUIZ_ADAPTOR_BRAND', 'Kasra' );
 
 if( is_admin() ) {
 	require_once( __DIR__ . '/backend.php' );
-	require_once( __DIR__ . '/admin-settings.php' );
-	require_once( __DIR__ . '/admin-settings-page.php' );
 } else {
 	require_once( __DIR__ . '/frontend.php' );
 }
-
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), function( $links ) {
-	ob_start(); ?>
-		<a href="<?= admin_url( 'options-general.php?page=quiz-options' ) ?>">Settings</a>
-	<?php $links[] = ob_get_clean();
-	return $links;
-});
